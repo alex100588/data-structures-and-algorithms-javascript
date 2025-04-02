de1f781f0d704103e587bc53baaa97eb46331c3f
alex100588 <alexandru_ionasc@yahoo.com>
2025-04-02 17:15:33 +0300

    O(n)

--- BIG O/script.js
+++ BIG O/script.js
@@ -1,9 +1,19 @@
 const nemo = ["alex", 'nemo']
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'bloat', 'nigel', 'squirt', 'darla']
 
+const large = new Array(1000000).fill('Alex')
+
+// O(n) --> linear time
 const findNemo = (array) =>{
+    // let time0 = performance.now()
     for (let i = 0; i < array.length; i++) {
         if(array[i] === 'nemo') console.log(`Found ${array[i]}`);
     }
+    // let time1 = performance.now()
+    // console.log(`Call to find nemo took ${time1 - time0} miliseconds`);
+    
 }
 
-findNemo(nemo)
+// findNemo(nemo)
+// findNemo(everyone)
+findNemo(large) 
